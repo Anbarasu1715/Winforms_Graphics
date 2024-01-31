f51e5f9fb14f2f3c2c959f128c22750287eb9403
ANBARASU M <anbarasu47442003@gmail.com>
2024-01-31 18:02:56 +0530

    31/01/2024

--- LucidLogo/LucidLogo/LogoTransUC.cs
+++ LucidLogo/LucidLogo/LogoTransUC.cs
@@ -30,7 +30,7 @@ namespace LucidLogo
         private int transitionCount = 0;
         private int minPoint;
         private int X1=0, Y1 = 0;
-        private int logoSize;
+        private int size;
 
         private void LoadingTrans() {
 
@@ -164,9 +164,23 @@ namespace LucidLogo
             g = e.Graphics;
             g.SmoothingMode = SmoothingMode.HighQuality;
             DoubleBuffered = true;
-            
+
+            StringFormat sf = new StringFormat
+            {
+                Alignment = StringAlignment.Near,
+                LineAlignment = StringAlignment.Center
+            };
+
+
+            StringFormat sf1 = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
 
             Brush brush = new SolidBrush(Color.FromArgb(13, 98, 177));
+            Brush letterBrush1 = new SolidBrush(Color.FromArgb(13, 98, 177));
+            Brush letterBrush2 = new SolidBrush(Color.FromArgb(0, 181, 226));
 
             //g.FillPolygon(brush,Poly1);
             //g.FillPolygon(brush,Poly2);
@@ -178,6 +192,9 @@ namespace LucidLogo
             //g.FillPolygon(brush,Poly8);
 
 
+            Font lucidFontFormat = new Font(new FontFamily("Segoe UI"), size / 2, FontStyle.Bold | FontStyle.Italic);
+            Font imagingFontFormat = new Font(new FontFamily("Segoe UI"), size / 3, FontStyle.Bold | FontStyle.Italic);
+
             foreach (Point[] point in fixedPoints) {
                 g.FillPolygon(brush,point);
             }
@@ -185,22 +202,9 @@ namespace LucidLogo
                 g.FillPolygon(brush,points[currentPoly]);
             }
 
-            Brush b1 = new SolidBrush(Color.FromArgb(13, 98, 177));
-            Brush b2 = new SolidBrush(Color.FromArgb(0, 181, 226));
 
-            StringFormat lucidSFormat = new StringFormat();
-            lucidSFormat.Alignment = StringAlignment.Near;
-            lucidSFormat.LineAlignment = StringAlignment.Center;
-
-            StringFormat imagingSFormat = new StringFormat();
-            imagingSFormat.Alignment = StringAlignment.Center;
-            imagingSFormat.LineAlignment = StringAlignment.Center;
-
-            Font f1 = new Font(new FontFamily("Segoe UI"), logoSize / 2, FontStyle.Bold | FontStyle.Italic);
-            Font f2 = new Font(new FontFamily("Segoe UI"), logoSize / 3, FontStyle.Bold | FontStyle.Italic);
-
-            e.Graphics.DrawString("Lucid", f1, b1, new Rectangle(X1 + logoSize, Y1, logoSize * 3, logoSize / 2), lucidSFormat);
-            e.Graphics.DrawString("Imaging", f2, b2, new Rectangle(X1 + logoSize, Y1 + logoSize / 2, logoSize * 3, logoSize / 2), imagingSFormat);
+            e.Graphics.DrawString("Lucid", lucidFontFormat, letterBrush1, new Rectangle(X1 + size, Y1, size * 3, size / 2), sf);
+            e.Graphics.DrawString("Imaging", imagingFontFormat, letterBrush2, new Rectangle(X1 + size, Y1 + size / 2, size * 3, size / 2), sf1);
 
 
             brush.Dispose();
@@ -209,90 +213,90 @@ namespace LucidLogo
 
         private void initialCoordinates() {
             Point[] Poly1 = {
-                new Point(((logoSize*110)/304)+X1,(logoSize * 46) / 286 +Y1),
-                new Point(((logoSize*151)/304)+X1,(logoSize *  5) /  286  +Y1),
-                new Point(((logoSize*173)/304)+X1,(logoSize * 26) / 286 +Y1),
-                new Point(((logoSize*170)/304)+X1,(logoSize * 101) / 286+Y1)
+                new Point(((size*110)/304)+X1,(size * 46) / 286 +Y1),
+                new Point(((size*151)/304)+X1,(size *  5) /  286  +Y1),
+                new Point(((size*173)/304)+X1,(size * 26) / 286 +Y1),
+                new Point(((size*170)/304)+X1,(size * 101) / 286+Y1)
             };
             points.Add(Poly1);
 
             Point[] Poly2 = {
-                new Point((logoSize*176)/304+X1,(logoSize*29)/286+Y1),
-                new Point((logoSize*215)/304+X1,(logoSize*68)/286+Y1),
-                new Point((logoSize*179)/304+X1,(logoSize*101)/286+Y1)
+                new Point((size*176)/304+X1,(size*29)/286+Y1),
+                new Point((size*215)/304+X1,(size*68)/286+Y1),
+                new Point((size*179)/304+X1,(size*101)/286+Y1)
             };
             points.Add(Poly2);
 
             Point[] Poly3 = {
-                new Point((logoSize*218)/304+X1,(logoSize*71)/286+Y1),
-                new Point((logoSize*257)/304+X1,(logoSize*109)/286+Y1),
-                new Point((logoSize*185)/304+X1,(logoSize*106)/286+Y1)
+                new Point((size*218)/304+X1,(size*71)/286+Y1),
+                new Point((size*257)/304+X1,(size*109)/286+Y1),
+                new Point((size*185)/304+X1,(size*106)/286+Y1)
             };
             points.Add(Poly3);
 
             Point[] Poly4 = {
-                new Point((logoSize*185)/304+X1,(logoSize*115)/286+Y1),
-                new Point((logoSize*259)/304+X1,(logoSize*112)/286+Y1),
-                new Point((logoSize*286)/304+X1,(logoSize*139)/286+Y1),
-                new Point((logoSize*280)/304+X1,(logoSize*142)/286+Y1),
-                new Point((logoSize*273)/304+X1,(logoSize*145)/286+Y1),
-                new Point((logoSize*265)/304+X1,(logoSize*149)/286+Y1),
-                new Point((logoSize*253)/304+X1,(logoSize*156)/286+Y1),
-                new Point((logoSize*241)/304+X1,(logoSize*164)/286+Y1),
-                new Point((logoSize*236)/304+X1,(logoSize*168)/286+Y1)
+                new Point((size*185)/304+X1,(size*115)/286+Y1),
+                new Point((size*259)/304+X1,(size*112)/286+Y1),
+                new Point((size*286)/304+X1,(size*139)/286+Y1),
+                new Point((size*280)/304+X1,(size*142)/286+Y1),
+                new Point((size*273)/304+X1,(size*145)/286+Y1),
+                new Point((size*265)/304+X1,(size*149)/286+Y1),
+                new Point((size*253)/304+X1,(size*156)/286+Y1),
+                new Point((size*241)/304+X1,(size*164)/286+Y1),
+                new Point((size*236)/304+X1,(size*168)/286+Y1)
             };
             points.Add(Poly4);
 
             Point[] Poly5 = {
-                new Point((logoSize*179)/304+X1,(logoSize*121)/286+Y1),
-                new Point((logoSize*232)/304+X1,(logoSize*170)/286+Y1),
-                new Point((logoSize*225)/304+X1,(logoSize*176)/286+Y1),
-                new Point((logoSize*216)/304+X1,(logoSize*183)/286+Y1),
-                new Point((logoSize*208)/304+X1,(logoSize*191)/286+Y1),
-                new Point((logoSize*200)/304+X1,(logoSize*199)/286+Y1),
-                new Point((logoSize*190)/304+X1,(logoSize*211)/286+Y1),
-                new Point((logoSize*176)/304+X1,(logoSize*230)/286+Y1),
+                new Point((size*179)/304+X1,(size*121)/286+Y1),
+                new Point((size*232)/304+X1,(size*170)/286+Y1),
+                new Point((size*225)/304+X1,(size*176)/286+Y1),
+                new Point((size*216)/304+X1,(size*183)/286+Y1),
+                new Point((size*208)/304+X1,(size*191)/286+Y1),
+                new Point((size*200)/304+X1,(size*199)/286+Y1),
+                new Point((size*190)/304+X1,(size*211)/286+Y1),
+                new Point((size*176)/304+X1,(size*230)/286+Y1),
             };
             points.Add(Poly5);
 
             Point[] Poly6 = {
-                new Point((logoSize*173)/304+X1,(logoSize*233)/286+Y1),
-                new Point((logoSize*166)/304+X1,(logoSize*245)/286+Y1),
-                new Point((logoSize*161)/304+X1,(logoSize*254)/286+Y1),
-                new Point((logoSize*157)/304+X1,(logoSize*263)/286+Y1),
-                new Point((logoSize*151)/304+X1,(logoSize*274)/286+Y1),
-                new Point((logoSize*145)/304+X1,(logoSize*261)/286+Y1),
-                new Point((logoSize*141)/304+X1,(logoSize*252)/286+Y1),
-                new Point((logoSize*137)/304+X1,(logoSize*244)/286+Y1),
-                new Point((logoSize*132)/304+X1,(logoSize*236)/286+Y1),
-                new Point((logoSize*129)/304+X1,(logoSize*230)/286+Y1),
-                new Point((logoSize*124)/304+X1,(logoSize*223)/286+Y1),
-                new Point((logoSize*119)/304+X1,(logoSize*215)/286+Y1),
-                new Point((logoSize*112)/304+X1,(logoSize*206)/286+Y1),
-                new Point((logoSize*98)/304 +X1,(logoSize*190)/286+Y1),
-                new Point((logoSize*170)/304+X1,(logoSize*122)/286+Y1)
+                new Point((size*173)/304+X1,(size*233)/286+Y1),
+                new Point((size*166)/304+X1,(size*245)/286+Y1),
+                new Point((size*161)/304+X1,(size*254)/286+Y1),
+                new Point((size*157)/304+X1,(size*263)/286+Y1),
+                new Point((size*151)/304+X1,(size*274)/286+Y1),
+                new Point((size*145)/304+X1,(size*261)/286+Y1),
+                new Point((size*141)/304+X1,(size*252)/286+Y1),
+                new Point((size*137)/304+X1,(size*244)/286+Y1),
+                new Point((size*132)/304+X1,(size*236)/286+Y1),
+                new Point((size*129)/304+X1,(size*230)/286+Y1),
+                new Point((size*124)/304+X1,(size*223)/286+Y1),
+                new Point((size*119)/304+X1,(size*215)/286+Y1),
+                new Point((size*112)/304+X1,(size*206)/286+Y1),
+                new Point((size*98)/304 +X1,(size*190)/286+Y1),
+                new Point((size*170)/304+X1,(size*122)/286+Y1)
             };
             points.Add(Poly6);
 
             Point[] Poly7 = {
-                new Point((logoSize*96)/304+X1,(logoSize*188)/286+Y1),
-                new Point((logoSize*83)/304+X1,(logoSize*177)/286+Y1),
-                new Point((logoSize*74)/304+X1,(logoSize*170)/286+Y1),
-                new Point((logoSize*67)/304+X1,(logoSize*165)/286+Y1),
-                new Point((logoSize*59)/304+X1,(logoSize*160)/286+Y1),
-                new Point((logoSize*51)/304+X1,(logoSize*155)/286+Y1),
-                new Point((logoSize*43)/304+X1,(logoSize*151)/286+Y1),
-                new Point((logoSize*33)/304+X1,(logoSize*146)/286+Y1),
-                new Point((logoSize*16)/304+X1,(logoSize*139)/286+Y1),
-                new Point((logoSize*42)/304+X1,(logoSize*113)/286+Y1),
-                new Point((logoSize*164)/304+X1,(logoSize*115)/286+Y1)
+                new Point((size*96)/304+X1,(size*188)/286+Y1),
+                new Point((size*83)/304+X1,(size*177)/286+Y1),
+                new Point((size*74)/304+X1,(size*170)/286+Y1),
+                new Point((size*67)/304+X1,(size*165)/286+Y1),
+                new Point((size*59)/304+X1,(size*160)/286+Y1),
+                new Point((size*51)/304+X1,(size*155)/286+Y1),
+                new Point((size*43)/304+X1,(size*151)/286+Y1),
+                new Point((size*33)/304+X1,(size*146)/286+Y1),
+                new Point((size*16)/304+X1,(size*139)/286+Y1),
+                new Point((size*42)/304+X1,(size*113)/286+Y1),
+                new Point((size*164)/304+X1,(size*115)/286+Y1)
             };
             points.Add(Poly7);
 
             Point[] Poly8 = {
-                new Point((logoSize*108)/304+X1,(logoSize*47)/286+Y1),
-                new Point((logoSize*164)/304+X1,(logoSize*107)/286+Y1),
-                new Point((logoSize*45)/304 +X1,(logoSize*111)/286+Y1),
+                new Point((size*108)/304+X1,(size*47)/286+Y1),
+                new Point((size*164)/304+X1,(size*107)/286+Y1),
+                new Point((size*45)/304 +X1,(size*111)/286+Y1),
             };
             points.Add(Poly8);
 
@@ -305,17 +309,17 @@ namespace LucidLogo
             initialCoordinates();
             if (Height * 4 <= Width)
             {
-                logoSize = Height;
+                size = Height;
                 X1 = (Width - Height * 4) / 2;
                 Y1 = 0;
             }
             else
             {
-                logoSize = Width / 4;
+                size = Width / 4;
                 X1 = 0;
-                Y1 = (Height - logoSize) / 2;
+                Y1 = (Height - size) / 2;
             }
-            X1 = X1 + logoSize / 4;
+            X1 = X1 + size / 4;
             minPoint = Math.Min(Width, Height);
             //LogoTransUC_Resize(sender, e);
         }
@@ -335,17 +339,17 @@ namespace LucidLogo
 
             if (Height * 4 <= Width)
             {
-                logoSize = Height;
+                size = Height;
                 X1 = (Width - Height * 4) / 2;
                 Y1 = 0;
             }
             else
             {
-                logoSize = Width / 4;
+                size = Width / 4;
                 X1 = 0;
-                Y1 = (Height - logoSize) / 2;
+                Y1 = (Height - size) / 2;
             }
-            X1 = X1 + logoSize / 4;
+            X1 = X1 + size / 4;
         }
     }
 }
